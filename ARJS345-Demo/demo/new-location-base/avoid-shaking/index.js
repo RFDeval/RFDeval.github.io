b30d508dc01504c04b02175d0a3e2d2d97c0aaf3
RFDeval <RFDeval@gmail.com>
2023-05-09 13:38:36 +0800

    test

--- ARJS345-Demo/demo/new-location-base/avoid-shaking/index.js
+++ ARJS345-Demo/demo/new-location-base/avoid-shaking/index.js
@@ -1,8 +1,9 @@
 window.onload = () => {
     let testEntitiesAdded = false;
-    alert('If testing the lat/lon manual input on a mobile device, please turn off your GPS to avoid the real location being detected.');
+    //alert('If testing the lat/lon manual input on a mobile device, please turn off your GPS to avoid the real location being detected.');
     const el = document.querySelector("[gps-new-camera]");
     el.addEventListener("gps-camera-update-position", e => {
+        alert(1)
         if(!testEntitiesAdded) {
             document.getElementById("gpsLab").innerHTML="gps is: " + e.detail.position.latitude + "," + e.detail.position.longitude;
             //alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
