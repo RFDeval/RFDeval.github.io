b03319cda481a1751ef5e3c6b969fdabca772a0a
RFDeval <RFDeval@gmail.com>
2023-05-09 14:17:23 +0800

    realtime gps

--- ARJS345-Demo/demo/new-location-base/avoid-shaking/index.js
+++ ARJS345-Demo/demo/new-location-base/avoid-shaking/index.js
@@ -3,7 +3,6 @@ window.onload = () => {
     //alert('If testing the lat/lon manual input on a mobile device, please turn off your GPS to avoid the real location being detected.');
     const el = document.querySelector("[gps-new-camera]");
     el.addEventListener("gps-camera-update-position", e => {
-        alert(1)
         if(!testEntitiesAdded) {
             document.getElementById("gpsLab").innerHTML="gps is: " + e.detail.position.latitude + "," + e.detail.position.longitude;
             //alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
