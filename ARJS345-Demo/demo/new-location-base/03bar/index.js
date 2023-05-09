fbe0e90dfe512ced515ad379a33cfb08e3ad582b
RFDeval <RFDeval@gmail.com>
2023-05-09 22:03:35 +0800

    test

--- ARJS345-Demo/demo/new-location-base/03bar/index.js
+++ ARJS345-Demo/demo/new-location-base/03bar/index.js
@@ -2,10 +2,20 @@ window.onload = () => {
     const el = document.querySelector("[gps-new-camera]");
     el.addEventListener("gps-camera-update-position", e => {
         document.getElementById("gpsLab").innerHTML = "gps is: " + e.detail.position.latitude + "," + e.detail.position.longitude;
+       
+        document.getElementById("en1").setAttribute('gps-new-entity-place', {
+            latitude: e.detail.position.latitude + 0.001,
+            longitude: e.detail.position.longitude
+        });
+
+       
         document.getElementById("bar1").setAttribute('gps-new-entity-place', {
             latitude: e.detail.position.latitude + 0.001,
             longitude: e.detail.position.longitude
         });
+
+
+
         document.getElementById("bar2").setAttribute('gps-new-entity-place', {
             latitude: e.detail.position.latitude + 0.001,
             longitude: e.detail.position.longitude + 0.001
